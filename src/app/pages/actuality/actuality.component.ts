96bfae9b6647d6fc3f55dd3d0e5714b6be170151
jim <jimmyrajoelinarivo@gmail.com>
2021-08-08 13:31:58 +0300

    Intégration actuality

--- src/app/pages/actuality/actuality.component.ts
+++ src/app/pages/actuality/actuality.component.ts
@@ -1,3 +1,4 @@
+import { ControllerService } from './../../services/controller.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActualityComponent implements OnInit {
 
-  constructor() { }
+  constructor(public ctrl: ControllerService) {}
 
   ngOnInit(): void {
   }
